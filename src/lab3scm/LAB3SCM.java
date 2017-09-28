95bb41716b352ef8e89dc3c73b2a34f95ddae8fb
AmelDarwish <32238305+AmelDarwish@users.noreply.github.com>
2017-09-28 15:28:07 +0400

    Update LAB3SCM.java

--- src/lab3scm/LAB3SCM.java
+++ src/lab3scm/LAB3SCM.java
@@ -23,9 +23,7 @@ public class LAB3SCM {
                 System.out.println("This is Nour's Feature!!");
     }
         public void AddFeatureAmel (){
-            while (true)
-            {
-                System.out.printlln("This is Amel's Feature!!");
-            }
+
+                System.out.println("This is Amel's Feature!!");
     }
 }
